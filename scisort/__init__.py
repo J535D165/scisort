0c9bf2a9f7e544e82aa09d939d3c85b2745dc4fd
Jonathan de Bruin <jonathandebruinos@gmail.com>
2022-10-10 22:47:09 +0200

    Improve version control

--- scisort/__init__.py
+++ scisort/__init__.py
@@ -1,8 +1,12 @@
-from setuptools_scm import get_version
+try:
+    from scisort._version import __version__
+    from scisort._version import __version_tuple__
+except ImportError:
+    __version__ = "0.0.0"
+    __version_tuple__ = (0, 0, 0)
+
 
 from scisort.keygen import scisort_keygen
 from scisort.keygen import scisort_keygen_pandas
 
 __all__ = ["scisort_keygen", "scisort_keygen_pandas"]
-
-__version__ = get_version()
